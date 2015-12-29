30781cb0281f615be735423ef249ff450de12e0f
Pete Considine <pete.considine@gmail.com>
2015-12-28 16:41:37 -0800

    Signed-off-by: Pete Considine <pete.considine@gmail.com>

--- getJSON Practice Files/js/main.js
+++ getJSON Practice Files/js/main.js
@@ -1,18 +1,30 @@
-var userData = [];
-
-function test(data, x) {
-	x = data;
-	console.log(x)
-}
-
-$.ajax({
-	type: "GET",
-	url: "./MOCK_DATA.js",
-	mimeType: "application/json",
-	success: function(data){
-		test(data, userData);
-	},
-	error: function(error) {
-		console.log("No data received");
-	}
+$(document).ready(function() {	
+	$("#scriptText").html("Worldwide users");
+	
+	$("#submit").click(function() {
+		$.ajax({
+			type: "GET",
+			url: "./MOCK_DATA.json",
+			mimeType: "application/json",
+			success: function(data){
+				var userCountry = $("#userCountry").val();
+				var newData = {};
+				
+				var userData = data.users.filter(function(element, index, array) {
+					if (element.country == userCountry) {
+						newData[element.id] = element;
+					}
+				});
+				
+				userData = {"users":{newData}};
+				
+				var source = $("#HBtemplate").html();
+				var template = Handlebars.compile(source);
+				$("#scriptText").html(template(userData));
+			},
+			error: function(error) {
+				console.log("No data received");
+			}
+		});
+	});
 });
