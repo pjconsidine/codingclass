7ab27a96d45ed4acb758f79b9d71eaf94c488edb
Pete Considine <pete.considine@gmail.com>
2016-01-07 23:33:10 +0000

    still doesn't work, but backing it up anyway.

--- WeatherApp/js/main.js
+++ WeatherApp/js/main.js
@@ -1,15 +1,16 @@
 $(function() {
 	$(window).load(function() {
+		
 		var Connect = new XMLHttpRequest();
-		Connect.open("GET", "./all_xml/41S47.xml", false);
+		Connect.open("GET", "./all_xml/42S39.xml", false);
 		Connect.setRequestHeader("Content-Type", "text/xml");
 		Connect.send(null);
 		
 		var Response = Connect.responseXML;
 		var MainEntries = Response.childNodes[1];
 		var SubEntries = MainEntries.childNodes;
-		var tempF = SubEntries[21].innerHTML;
-		var tempC = SubEntries[23].innerHTML;
+		// var tempF = SubEntries[21].innerHTML;
+		// var tempC = SubEntries[23].innerHTML;
 				
 		if (SubEntries.length > 0) {
 			ProcessEntries(SubEntries);
@@ -19,13 +20,15 @@ $(function() {
 
 	});
 	
-	var MenuData = "";
+	var MenuData = "<table class='table'><thead><tr><th>Node Name</th><th>Node Value</th></tr></thead>";
 	
 	function ProcessEntries(Nodes) {
 		for (var i = 1; i < Nodes.length; i+=2) {
 			var ThisNode = Nodes[i];
-			console.log(ThisNode);
-			MenuData += "<li><b>" + ThisNode.nodeName + ":</b> " + ThisNode.innerHTML + "</li>";
+			MenuData += "<tbody>";
+			MenuData += "<tr><td>" + ThisNode.nodeName + "</td><td>" + ThisNode.innerHTML + "</td></tr>";
+			MenuData += "</tbody>";
 		}
+		MenuData += "</table>";
 	}
 });
