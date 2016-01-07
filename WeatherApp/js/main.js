b06b33d54e5f4dda153f54b0d3577af52e52d0a6
Pete Considine <pete.considine@gmail.com>
2016-01-07 09:08:04 -0800

    Signed-off-by: Pete Considine <pete.considine@gmail.com>

--- WeatherApp/js/main.js
+++ WeatherApp/js/main.js
@@ -1,32 +1,31 @@
-function loadXMLDoc() {
-	var xmlhttp;
+$(function() {
+	$(window).load(function() {
+		var Connect = new XMLHttpRequest();
+		Connect.open("GET", "./all_xml/41S47.xml", false);
+		Connect.setRequestHeader("Content-Type", "text/xml");
+		Connect.send(null);
+		
+		var Response = Connect.responseXML;
+		var MainEntries = Response.childNodes[1];
+		var SubEntries = MainEntries.childNodes;
+		var tempF = SubEntries[21].innerHTML;
+		var tempC = SubEntries[23].innerHTML;
+				
+		if (SubEntries.length > 0) {
+			ProcessEntries(SubEntries);
+		}
+		
+		$("#result-text").html(MenuData);
 
-	if (window.XMLHttpRequest) {
-		// code for IE7+, Firefox, Chrome, Opera, Safari
-		xmlhttp = new XMLHttpRequest();
-	} else {
-		// code for IE6, IE5
-		xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
-	}
-
-	xmlhttp.onreadystatechange = function() {
-		if (xmlhttp.readyState == XMLHttpRequest.DONE ) {
-		   if(xmlhttp.status == 200){
-			   document.getElementById("results-text").innerHTML = xmlhttp.responseText;
-		   }
-		   else if(xmlhttp.status == 400) {
-			  alert('There was an error 400')
-		   }
-		   else {
-			   alert('something other than 200 was returned')
-		   }
+	});
+	
+	var MenuData = "";
+	
+	function ProcessEntries(Nodes) {
+		for (var i = 1; i < Nodes.length; i+=2) {
+			var ThisNode = Nodes[i];
+			console.log(ThisNode);
+			MenuData += "<li><b>" + ThisNode.nodeName + ":</b> " + ThisNode.innerHTML + "</li>";
 		}
 	}
-
-	xmlhttp.open("GET", "http://api.weatherunlocked.com/api/current/47.6358,-122.3438?app_id=8c4697ce&app_key=1dde4656844165ba667bfa947879e958", true);
-	xmlhttp.send();
-}
-
-
-
-// Flickr API key: 9ca681b6d1ccdd6e384846e8011d48c4
+});
