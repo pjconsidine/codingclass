bd33d1af3e0853deaea8657c707245273b75bef0
Pete Considine <pete.considine@gmail.com>
2016-01-07 13:22:15 -0800

    Signed-off-by: Pete Considine <pete.considine@gmail.com>

--- WeatherApp/js/main.js
+++ WeatherApp/js/main.js
@@ -1,15 +1,13 @@
 $(function() {
 	$(window).load(function() {
 		var Connect = new XMLHttpRequest();
-		Connect.open("GET", "./all_xml/41S47.xml", false);
+		Connect.open("GET", "http://api.weatherunlocked.com/api/current/51.50,-0.12?app_id=8c4697ce&app_key=1dde4656844165ba667bfa947879e958", false);
 		Connect.setRequestHeader("Content-Type", "text/xml");
 		Connect.send(null);
 		
 		var Response = Connect.responseXML;
-		var MainEntries = Response.childNodes[1];
+		var MainEntries = Response.childNodes[0];
 		var SubEntries = MainEntries.childNodes;
-		var tempF = SubEntries[21].innerHTML;
-		var tempC = SubEntries[23].innerHTML;
 				
 		if (SubEntries.length > 0) {
 			ProcessEntries(SubEntries);
@@ -19,13 +17,15 @@ $(function() {
 
 	});
 	
-	var MenuData = "";
+	var MenuData = "<table class='table'><thead><tr><th>Node Name</th><th>Node Value</th></tr></thead>";
 	
 	function ProcessEntries(Nodes) {
 		for (var i = 1; i < Nodes.length; i+=2) {
 			var ThisNode = Nodes[i];
-			console.log(ThisNode);
-			MenuData += "<li><b>" + ThisNode.nodeName + ":</b> " + ThisNode.innerHTML + "</li>";
+			MenuData += "<tbody>";
+			MenuData += "<tr><td>" + ThisNode.nodeName + "</td><td>" + ThisNode.innerHTML + "</td></tr>";
+			MenuData += "</tbody>";
 		}
+		MenuData += "</table>";
 	}
 });
